7be9643133dc326819a23e67f7e008ab79d771bb
Werner Bogula <Bogula@gmail.com>
2023-01-10 15:24:14 +0100

    cool
    test

---
+++ aud.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from audio_recorder_streamlit import audio_recorder
+
+audio_bytes = audio_recorder(pause_threshold=1.0)
+if audio_bytes:
+    wav= st.audio(audio_bytes, format="audio/wav")
+    st.write(type(audio_bytes))
+    wav_file = open("audio.wav", "wb")
+    wav_file.write(audio_bytes)
