f5a00767486e429face7b8d044cb5c87550f7a67
Werner Bogula <Bogula@gmail.com>
2023-01-10 15:46:13 +0100

    Update aud.py

--- aud.py
+++ aud.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import whisper
 from audio_recorder_streamlit import audio_recorder
+
+
+# Whisper
+
+Model ="base"
+English = True
+
+model = whisper.load_model(f'{Model}{".en" if English else ""}')
+#system("say --rate=190 --voice=Anna Willkommen beim Mobility Genius.")
+#system("say --rate=190 --voice=Anna Willkommen beim Mobility Genius. Drück auf die Mikrofonbutton um was zu fragen") 
+    
 
 audio_bytes = audio_recorder(pause_threshold=1.0)
 if audio_bytes:
@@ -8,3 +20,4 @@ if audio_bytes:
     wav_file = open("audio.wav", "wb")
     wav_file.write(audio_bytes)
    
+
