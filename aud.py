a2c8e3269439ffb4094e6d61a16547249e36c02f
Werner Bogula <Bogula@gmail.com>
2023-01-10 15:38:47 +0100

    Update aud.py

--- aud.py
+++ aud.py
@@ -5,5 +5,4 @@ audio_bytes = audio_recorder(pause_threshold=1.0)
 if audio_bytes:
     wav= st.audio(audio_bytes, format="audio/wav")
     st.write(type(audio_bytes))
-    wav_file = open("audio.wav", "wb")
-    wav_file.write(audio_bytes)
+   
